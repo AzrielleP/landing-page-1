48e9ba9ca9caab8d0c4f6296b625c98e4becb4a1
Aze <azrielle101@gmail.com>
2020-09-03 21:44:53 +0800

    Formatted the files, added comments

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 const toggleMenu = (() => {
+  const burgerIcon = document.querySelector('.burger-icon');
+  const burgerIconContainer = document.querySelector('.burger-icon-container');
+  const nav = document.querySelector('.nav-links-container');
 
-    const burgerIcon = document.querySelector('.burger-icon');
-    const burgerIconContainer = document.querySelector('.burger-icon-container');
-    const nav = document.querySelector('.nav-links-container');
-
-    burgerIcon.addEventListener('click', ()=> {
-        nav.classList.toggle('expanded');
-        burgerIconContainer.classList.toggle('expanded');
-    })
-
-    
-    
+  burgerIcon.addEventListener('click', () => {
+    nav.classList.toggle('expanded');
+    burgerIconContainer.classList.toggle('expanded');
+  });
 })();
-
